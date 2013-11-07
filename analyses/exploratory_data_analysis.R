00222dda98005c7a6df3f5d4e57ebd64b3a20d54
joelchan <chozen86@gmail.com>
2013-11-07 16:54:15 -0500

    first ggplot scatterplot with regression line

--- analyses/exploratory_data_analysis.R
+++ analyses/exploratory_data_analysis.R
@@ -6,3 +6,10 @@ file = "/Users/jchan/Desktop/extsources.csv"
 
 #read in the data
 data = read.csv(file,header=T,sep=",")
+
+chart = ggplot(data, aes(x=LDA, y=human)) +
+  geom_point(shape=1, size=1.5) +
+  geom_smooth(method=lm) +
+  theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank(), panel.background = element_rect(fill='white',colour='grey'))
+
+ggsave(file="/Users/joelc/Desktop/LDA-ewaste.png",plot=graph, width=6, height=4)
