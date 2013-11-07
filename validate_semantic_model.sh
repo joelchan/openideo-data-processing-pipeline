b524ba620b37a5f625e4bb4e71c72e96e4294ad2
Joel Chan <chozen86@gmail.com>
2013-11-07 18:08:28 -0500

    estimate model for vector of K instead of just one K
    included for loop. next step is to write scripts to iterate over output
    files to estimate fit with human judgments.

--- validate_semantic_model.sh
+++ validate_semantic_model.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 PARENTDIR=/Users/joelc/Desktop/Dropbox/Research/Dissertation/OpenIDEO/Pipeline/
-
-# go to the mallet directory
-cd ${PARENTDIR}openideo-data-processing-pipeline/semantic-models/mallet-2.0.7
-
-NUMTOPICS=300
+NUMTOPICS=(12 20 30 40 50 60 70 80 90 100 150 200 300 400 500)
 INPUT=${PARENTDIR}SemanticModelData/mallet/inputs/openIDEO_CF1_DF50.mallet
-OUTPUT_KEYS=${PARENTDIR}SemanticModelData/mallet/outputs/openideo_CF1_DF50_${NUMTOPICS}_ASP_optim_keys.txt
-OUTPUT_DOCS=${PARENTDIR}SemanticModelData/mallet/outputs/openideo_CF1_DF50_${NUMTOPICS}_ASP_optim_composition.txt
 
-# train the topic model
-./bin/mallet train-topics --input $INPUT --num-topics $NUMTOPICS --output-topic-keys $OUTPUT_KEYS --output-doc-topics $OUTPUT_DOCS --use-symmetric-alpha false --show-topics-interval 100 --optimize-interval 10
+for NUMTOPIC in "${NUMTOPICS[@]}"
+do
+    
+    # make the names
+    OUTPUT_KEYS=${PARENTDIR}SemanticModelData/mallet/outputs/openideo_CF1_DF50_${NUMTOPIC}_ASP_optim_keys.txt
+    OUTPUT_DOCS=${PARENTDIR}SemanticModelData/mallet/outputs/openideo_CF1_DF50_${NUMTOPIC}_ASP_optim_composition.txt
+    
+    # train the topic model
+    cd ${PARENTDIR}openideo-data-processing-pipeline/semantic-models/mallet-2.0.7
+    ./bin/mallet train-topics --input $INPUT --num-topics $NUMTOPIC --output-topic-keys $OUTPUT_KEYS --output-doc-topics $OUTPUT_DOCS --use-symmetric-alpha false --show-topics-interval 100 --optimize-interval 10
+    
+    # reshape the output
+    cd ${PARENTDIR}openideo-data-processing-pipeline/model-post-processing
+    OUTPUT_DOCS_RESHAPED=${PARENTDIR}/Validation/LDA_CF1_DF50_ASP_opt10/sorted_openideo_CF1_DF50_${NUMTOPIC}_ASP_optim_composition.csv
+    python reshape_mallet_doctopiccomposition.py $OUTPUT_DOCS $OUTPUT_DOCS_RESHAPED $NUMTOPIC
 
-# reshape the output
-cd ${PARENTDIR}openideo-data-processing-pipeline/model-post-processing
+done
 
-OUTPUT_DOCS_RESHAPED=${PARENTDIR}/SemanticModelData/mallet/outputs/sorted_openideo_CF1_DF50_${NUMTOPICS}_ASP_optim_composition.csv
 
-python reshape_mallet_doctopiccomposition.py $OUTPUT_DOCS $OUTPUT_DOCS_RESHAPED $NUMTOPICS
+#Probably better to do something like this:
+#
+#for numtopic in NUMTOPIC:
+#    train model
+#    save output to directory
+#    
+#benchmark_continuous # will iterate over the output files in the directory
+    
