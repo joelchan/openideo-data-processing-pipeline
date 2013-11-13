5e1545fb75ddd05b42551ed86deb28b29e0e23f3
Joel Chan <chozen86@gmail.com>
2013-11-13 17:21:29 -0500

    junk changes (for new experiment)

--- validate_semantic_model.sh
+++ validate_semantic_model.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 PARENTDIR=/Users/joelc/Desktop/Dropbox/Research/Dissertation/OpenIDEO/Pipeline/
-OUTPUTDIR=/Users/joelc/Desktop/LDA_CF1_DF50_ASP_90_opt10/
+OUTPUTDIR=/Users/joelc/Desktop/LDA_CF0_DF0_ASP_opt0/
 
-NUMTOPICS=(12 25 50 100 150 200 300 400 500 600 700)
-INPUT=${PARENTDIR}SemanticModelData/mallet/inputs/openIDEO_CF1_DF50.mallet
+#NUMTOPICS=(12 25 50 100 150 200 300 400 500 600 700)
+NUMTOPICS=(12 25 50 200 500)
+INPUT=${PARENTDIR}SemanticModelData/mallet/inputs/openIDEO_CF0_DF0.mallet
 
 for NUMTOPIC in "${NUMTOPICS[@]}"
 do
     
     RUNS=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50)
+    #RUNS=(1 2 3 4 5 6 7 8 9 10)
     
     for RUN in "${RUNS[@]}"
     do
