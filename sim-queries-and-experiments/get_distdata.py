72356810a7ba5dbd9984d8471cd23ef9cf2bf8fc
joelchan <joc59@pitt.edu>
2013-11-19 16:05:07 -0500

    intermediate commit - now we slice out sub-slices by level

--- sim-queries-and-experiments/get_distdata.py
+++ sim-queries-and-experiments/get_distdata.py
@@ -38,40 +38,111 @@ def process_challenge(infilename, doc_topic_weights, doc_names, cb_indices, outd
     cbweights = doc_topic_weights[doc_names[cb_indices[0]]]
     currentindex = 0
     
-    debugfile = open("debugfile.txt",'w')
-    for seed in seeds:
-        seedweights = doc_topic_weights[seed]
-        seeddata = {"0_conceptname":seed}
-        for level in levels:
-            levelcosines_cb = []
-            levelcosines_con = []
-            fieldname_cb = "cb_" + str(levels.index(level)+1) + "_meandist_" + level
-            fieldname_con = "con_" + str(levels.index(level)+1) + "_meandist_" + level
-            while currentindex < len(pathdata) and pathdata[currentindex][0] == seed and pathdata[currentindex][2] == level:
-                query = pathdata[currentindex][1]
-                queryweights = doc_topic_weights[query]
-                levelcosines_cb.append(numpy.dot(cbweights,queryweights))
-                levelcosines_con.append(numpy.dot(seedweights,queryweights))
-                currentindex += 1
-            if len(levelcosines_cb) > 0: # what if we don't have any at this level?
-                levelcosinemean_cb = numpy.mean(levelcosines_cb)
-                levelcosinemean_con = numpy.mean(levelcosines_con)
-                seeddata[fieldname_cb] = levelcosinemean_cb
-                seeddata[fieldname_con] = levelcosinemean_con
-                for levelcosine in levelcosines_cb:
-                    towrite = "%s\t%s\t%s\t%.3f\n" %(seed,query,level,levelcosine)
-                    debugfile.write(towrite)
-            else:
-                seeddata[fieldname_cb] = "NaN"
-                seeddata[fieldname_con] = "NaN"
-            
-        sortedseeddata = collections.OrderedDict(sorted(seeddata.items(), key=lambda t:t[0]))
-        outdata.append(sortedseeddata)
-    debugfile.close()
+    # partition data
+    paths_immediate = []
+    paths_indirect_recent = []
+    paths_indirect_medium = []
+    paths_indirect_old = []
+    for path in pathdata:
+        pathlevel = path[2]
+        if pathlevel == "immediate":
+            paths_immediate.append(path)
+        elif pathlevel == "indirect-recent":
+            paths_indirect_recent.append(path)
+        elif pathlevel == "indirect-medium":
+            paths_indirect_medium.append(path)
+        else:
+            paths_indirect_old.append(path)
+                
+    #print paths_immediate
+    #print paths_indirect_recent
+    #print paths_indirect_medium
+    #print paths_indirect_old
+    with open("immediate.txt",'w') as f:
+        for path in paths_immediate:
+            for p in path:
+                f.write(p + "\t")
+            f.write("\n")
+    f.close()
+    with open("indirect-recent.txt",'w') as f:   
+        for path in paths_indirect_recent:
+            for p in path:
+                f.write(p + "\t")
+            f.write("\n")
+    f.close()
+    with open("indirect-medium.txt",'w') as f:
+        for path in paths_indirect_medium:
+            for p in path:
+                f.write(p + "\t")
+            f.write("\n")
+    f.close()
+    with open("indirect-far.txt",'w') as f:
+        for path in paths_indirect_old:
+            for p in path:
+                f.write(p + "\t")
+            f.write("\n")
+    f.close()
+    
+    ##debugfile = open("debugfile.txt",'w')
+    #for seed in seeds:
+    #    seedweights = doc_topic_weights[seed]
+    #    seeddata = {"0_conceptname":seed}
+    #    # inspirations
+    #    for level in levels:
+    #        insp_levelcosines_cb = []
+    #        insp_levelcosines_con = []
+    #        fieldname_concept_cb = "concept_cb_" + str(levels.index(level)+1) + "_meandist_" + level
+    #        fieldname_concept_con = "concept_con_" + str(levels.index(level)+1) + "_meandist_" + level
+    #        while currentindex < len(pathdata) and pathdata[currentindex][0] == seed and pathdata[currentindex][2] == level:
+    #            if "_I-" in pathdata[currentindex][1]:
+    #                query = pathdata[currentindex][1]
+    #                queryweights = doc_topic_weights[query]
+    #                insp_levelcosines_cb.append(numpy.dot(cbweights,queryweights))
+    #                insp_levelcosines_con.append(numpy.dot(seedweights,queryweights))
+    #                currentindex += 1
+    #        if len(insp_levelcosines_cb) > 0: # what if we don't have any at this level?
+    #            insp_levelcosinemean_cb = numpy.mean(insp_levelcosines_cb)
+    #            insp_levelcosinemean_con = numpy.mean(insp_levelcosines_con)
+    #            seeddata[fieldname_insp_cb] = insp_levelcosinemean_cb
+    #            seeddata[fieldname_insp_con] = insp_levelcosinemean_con
+    #            #for levelcosine in levelcosines_cb:
+    #            #    towrite = "%s\t%s\t%s\t%.3f\n" %(seed,query,level,levelcosine)
+    #            #    debugfile.write(towrite)
+    #        else:
+    #            seeddata[fieldname_insp_cb] = "NaN"
+    #            seeddata[insp_fieldname_insp_con] = "NaN"
+    #    # concepts
+    #    for level in levels:
+    #        concept_levelcosines_cb = []
+    #        concept_levelcosines_con = []
+    #        fieldname_concept_cb = "concept_cb_" + str(levels.index(level)+1) + "_meandist_" + level
+    #        fieldname_concept_con = "concept_con_" + str(levels.index(level)+1) + "_meandist_" + level
+    #        while currentindex < len(pathdata) and pathdata[currentindex][0] == seed and pathdata[currentindex][2] == level:
+    #            if "_C-" in pathdata[currentindex][1]:
+    #                query = pathdata[currentindex][1]
+    #                queryweights = doc_topic_weights[query]
+    #                concept_levelcosines_cb.append(numpy.dot(cbweights,queryweights))
+    #                concept_levelcosines_con.append(numpy.dot(seedweights,queryweights))
+    #                currentindex += 1
+    #        if len(concept_levelcosines_cb) > 0: # what if we don't have any at this level?
+    #            concept_levelcosinemean_cb = numpy.mean(concept_levelcosines_cb)
+    #            concept_levelcosinemean_con = numpy.mean(concept_levelcosines_con)
+    #            seeddata[fieldname_concept_cb] = concept_levelcosinemean_cb
+    #            seeddata[fieldname_concept_con] = concept_levelcosinemean_con
+    #            #for levelcosine in levelcosines_cb:
+    #            #    towrite = "%s\t%s\t%s\t%.3f\n" %(seed,query,level,levelcosine)
+    #            #    debugfile.write(towrite)
+    #        else:
+    #            seeddata[fieldname_concept_cb] = "NaN"
+    #            seeddata[fieldname_concept_con] = "NaN"
+    #        
+    #    sortedseeddata = collections.OrderedDict(sorted(seeddata.items(), key=lambda t:t[0]))
+    #    outdata.append(sortedseeddata)
+    #debugfile.close()
 
 data_dir = argv[1]
 doctopicweightsfilename = argv[2]
-outfilename = argv[3]
+#outfilename = argv[3]
 
 # read in the doc-topic weights
 rawdoctopic = []
@@ -102,5 +173,5 @@ for d in os.listdir(data_dir):
         process_challenge(data_filename,doc_topic_weights, doc_names, cb_indices, outdata)
 
 #convert to dataframe and print out
-df = pandas.DataFrame(outdata)
-df.to_csv(outfilename)
+#df = pandas.DataFrame(outdata)
+#df.to_csv(outfilename)
