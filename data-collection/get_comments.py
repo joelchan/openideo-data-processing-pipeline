94a4ca13418569e4990d6a8b5170ce6fd0b9214a
Joel Chan <chozen86@gmail.com>
2013-12-23 22:33:43 -0500

    cleaned up method for grabbing author url, added more informative command-line runtime output
    also generally cleaned up the code - no more messy subsouping

--- data-collection/get_comments.py
+++ data-collection/get_comments.py
@@ -10,52 +10,48 @@
 # results file.
 
 from bs4 import BeautifulSoup
-from sys import argv
+from sys import argv, stdout
 import os
 
 def extract_comments(filename, docname, challengename):
 
-    # create the big soup of comments stuff, put in a list
-    soup = BeautifulSoup(open(filename))
-    comments = soup.find_all(class_="comment")
-
-    # iterate over the comments in the comments list
-    for comment in comments:
-        #print "Next subsoup..."
-        subsoup = BeautifulSoup(str(comment))
-
-        # get author URL - very hacky - would like to just get the right URL
-        links = []
-        for link in subsoup.find_all('a'):
-            #print link
-            if link.get('href') == "": #blank comment placeholder
-                links.append("blank/url")
-            else:
-                links.append(link.get('href'))
-        author_url = links[1]
-
-        # get comment date - very hacky at the moment, have to make lots of subsoups!
-        body = subsoup.find('div',class_="comment-body clearfix")
-        subsoup_body = BeautifulSoup(str(body))
-        datesoup = BeautifulSoup(str(subsoup_body.find(class_="comment-date")))
-        date = datesoup.get_text()
-    
-        # get comment full-text - very hacky, have to re-encode the full-text as ascii
-        # and strip away whitespace (newlines and tabs) to make for easier reading
-        textsoup = BeautifulSoup(str(subsoup_body.find(class_="comment-message")))
-        temptext = textsoup.get_text().strip()
-        temptext = temptext.encode('ascii', 'ignore')
-        textarr = temptext.splitlines()
-        text = ""
-        for t in textarr:
-            text = text + t.strip() + " "
-    
-        # print to resultsfile
-        if "profiles" in author_url:
-            temp = author_url.split("/")
-            author_url = temp[len(temp)-2]
-            towrite = "%s\t%s\t%s\t%s\t%s\n" %(challengename, docname, author_url, date, text)
-            results.write(towrite)	
+	docname = docname.replace(".html","")
+	# create the big soup of comments stuff, put in a list
+	soup = BeautifulSoup(open(filename),"html5lib") # CRITICAL THAT WE USE THE HTML5LIB parser, otherwise we'll lose data
+	comments = soup.find_all('div',class_="comment")
+	
+	# iterate over the comments in the comments list
+	for comment in comments:
+		
+		authorsoup = comment.find(class_="comment-author").find(class_="author-name")
+		
+		if authorsoup != None: #only proceed if it's a real author_url (i.e., actually a real comment, not just a placeholder for user-submitted comment) ALSO threw in the text soup earlier to catch that one file (food-production-consumption_C-149.html) that had a blank comment with author info but no text
+			
+			# get author_url
+			author_url = authorsoup.find("a").get("href")
+			
+			bodysoup = comment.find('div',class_="comment-body clearfix")
+			try:
+				# get comment full-text - very hacky, have to re-encode the full-text as ascii
+				# and strip away whitespace (newlines and tabs) to make for easier reading
+				textsoup = bodysoup.find(class_="comment-message")
+				temptext = textsoup.get_text().strip() #remove blank lines, whitespace, etc.
+				temptext = temptext.encode('ascii', 'ignore') #encode to ascii
+				textarr = temptext.splitlines() #split and join back together to enhance readability
+				text = ""
+				for t in textarr:
+					text = text + t.strip() + " "
+				
+				# get date		
+				datesoup = bodysoup.find(class_="comment-date")
+				date = datesoup.get_text()
+				
+			except:
+				print "Crikey! We've got a bug!"
+				continue
+				
+			# print out results
+			results.write("%s\t%s\t%s\t%s\t%s\n" %(challengename, docname, author_url, date, text))
 
 # get args from command-line
 HTML_dir = argv[1]
@@ -69,22 +65,17 @@ results.write("challenge\tdocument\tauthor_url\tdate\ttext\n")
 for d in os.listdir(HTML_dir):
 	dir_name = HTML_dir + d
 	if os.path.isdir(dir_name): #pesky .dsstore!!!!!!
-		for f in os.listdir(dir_name):
+		print "Processing %s..." %d
+		files = os.listdir(dir_name)
+		numfiles = len(files)-4
+		processedfiles = 0
+		for f in files:
 			if f.endswith(".html"):
-			    print "Getting comments from %s..." %f
-			    filename = dir_name + "/" + f #make the filename
-			    extract_comments(filename,f,d) #extract its comments data and dump in the results file
-
-#for root, dirs, files in os.walk(HTML_dir):
-#	for d in dirs:
-#		print os.path.abspath(d)
-    #for name in files: # for each HTML file
-    #    if name.endswith(".html"):
-    #        print "Getting comments from %s..." %os.path.abspath(name)
-    #        #filename = HTML_dir + name #make the filename
-    #        extract_comments(os.path.abspath(name)) 
-
+				filename = dir_name + "/" + f #make the filename
+				#print "Processing %s..." %f
+				extract_comments(filename,f,d) #extract its comments data and dump in the results file
+				processedfiles +=1
+				stdout.write("\t%d of %d files processed...\r" %(processedfiles, numfiles))
+				stdout.flush()
+		print "\t%d of %d files processed...\r" %(processedfiles, numfiles)
 results.close()
-	
-	
-	
